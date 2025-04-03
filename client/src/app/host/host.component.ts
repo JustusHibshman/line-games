6f9f4fd6bf600bd536aa32c37566dc6ae3ee4a4e
Justus Hibshman <justus.i.hibshman@proton.me>
2025-04-03 18:04:54 -0400

    added test toggle switch to host page

--- client/src/app/host/host.component.ts
+++ client/src/app/host/host.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavButtonComponent } from '@local-components/nav-button/nav-button.component';
+import { ToggleSwitchComponent } from '@local-components/toggle-switch/toggle-switch.component';
 
 @Component({
   selector: 'app-host',
-  imports: [NavButtonComponent],
+  imports: [NavButtonComponent, ToggleSwitchComponent],
   templateUrl: './host.component.html',
   styleUrl: './host.component.scss'
 })
