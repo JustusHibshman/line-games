aa5673b68acb3461807a2901eac9179b8a460e9e
Justus Hibshman <justus.i.hibshman@proton.me>
2025-04-04 14:44:04 -0400

    Added logical functionality and styling to the toggle switch component

--- client/src/app/components/toggle-switch/toggle-switch.component.ts
+++ client/src/app/components/toggle-switch/toggle-switch.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input, OnInit, output, signal } from '@angular/core';
 
 @Component({
   selector: 'app-toggle-switch',
@@ -6,6 +6,23 @@ import { Component } from '@angular/core';
   templateUrl: './toggle-switch.component.html',
   styleUrl: './toggle-switch.component.scss'
 })
-export class ToggleSwitchComponent {
+export class ToggleSwitchComponent implements OnInit {
+    defaultValue = input<boolean>(false);
+    disable      = input<boolean>(false);
+    value        = signal<boolean>(false);
+    choice       = output<boolean>();
 
+    ngOnInit(): void {
+        this.value.set(this.defaultValue());
+        this.choice.emit(this.value());
+    }
+
+    toggle(): void {
+        if (this.disable()) {
+            this.value.set(this.defaultValue());
+        } else {
+            this.value.set(!this.value());
+        }
+        this.choice.emit(this.value());
+    }
 }
--- client/src/app/host/host.component.ts
+++ client/src/app/host/host.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { NavButtonComponent } from '@local-components/nav-button/nav-button.component';
 import { ToggleSwitchComponent } from '@local-components/toggle-switch/toggle-switch.component';
 
@@ -9,5 +9,19 @@ import { ToggleSwitchComponent } from '@local-components/toggle-switch/toggle-sw
   styleUrl: './host.component.scss'
 })
 export class HostComponent {
+    gravity:       boolean = false;
+    allowCaptures = signal<boolean>(false);
+    winByCaptures: boolean = false;
 
+    setGravity(event: boolean): void {
+        this.gravity = event;
+    }
+
+    setAllowCaptures(event: boolean): void {
+        this.allowCaptures.set(event);
+    }
+
+    setWinByCaptures(event: boolean): void {
+        this.winByCaptures = event;
+    }
 }
