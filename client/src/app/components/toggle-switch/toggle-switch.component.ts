a2d8f668120a171c432222a40e0a575246b8d1f0
Justus Hibshman <justus.i.hibshman@proton.me>
2025-04-04 17:29:30 -0400

    Added two-way data binding to simplify toggle switch

--- client/src/app/components/toggle-switch/toggle-switch.component.ts
+++ client/src/app/components/toggle-switch/toggle-switch.component.ts
@@ -1,28 +1,37 @@
-import { Component, input, OnInit, output, signal } from '@angular/core';
+import { Component, input, OnChanges, OnInit, output, signal, SimpleChanges } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-toggle-switch',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './toggle-switch.component.html',
   styleUrl: './toggle-switch.component.scss'
 })
-export class ToggleSwitchComponent implements OnInit {
+export class ToggleSwitchComponent implements OnInit, OnChanges {
     defaultValue = input<boolean>(false);
     disable      = input<boolean>(false);
-    value        = signal<boolean>(false);
+    theValue     = signal<boolean>(false);
     choice       = output<boolean>();
 
     ngOnInit(): void {
-        this.value.set(this.defaultValue());
-        this.choice.emit(this.value());
+        this.theValue.set(this.defaultValue());
+        this.choice.emit(this.theValue());
     }
 
-    toggle(): void {
-        if (this.disable()) {
-            this.value.set(this.defaultValue());
-        } else {
-            this.value.set(!this.value());
+    ngOnChanges(changes: SimpleChanges): void {
+        if ((changes['disable'] && changes['disable'].currentValue) || 
+                (changes['defaultValue'] && 
+                 changes['defaultValue'].currentValue != changes['defaultValue'].previousValue)) {
+            /* If disabled or the default value changes */
+            if (this.theValue() != this.defaultValue()) {
+                /* If we actually need to change */
+                this.theValue.set(this.defaultValue());
+                this.choice.emit(this.theValue());
+            }
         }
-        this.choice.emit(this.value());
+    }
+
+    changeFn(): void {
+        this.choice.emit(this.theValue());
     }
 }
