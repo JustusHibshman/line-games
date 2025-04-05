b90d4b890502077a9c78cca224b36d7750a092c4
Justus Hibshman <justus.i.hibshman@proton.me>
2025-04-04 20:34:28 -0400

    Made text-box to scale with length of input

--- client/src/app/components/text-box/text-box.component.ts
+++ client/src/app/components/text-box/text-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, model } from '@angular/core';
+import { Component, computed, input, model } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -10,4 +10,24 @@ import { FormsModule } from '@angular/forms';
 export class TextBoxComponent {
     maxLength = input<number>(20);
     text = model<string>();
+
+    measureText(t: string | undefined): number {
+        if (t === undefined) {
+            return 0;
+        }
+        let canvas = <HTMLCanvasElement> document.getElementById("canvas");
+        let ctx = canvas.getContext("2d");
+        if (ctx === null) {
+            return 0;
+        }
+        ctx.font = "24px Arial";
+        /* The "."'s assure that any whitespace at the front and back of t is measured. */
+        let stats = ctx.measureText("." + t + ".");
+        let dots = ctx.measureText("..");
+        let full = stats.actualBoundingBoxRight + stats.actualBoundingBoxLeft;
+        /* The 2 is for the cursor */
+        return 2 + full - (dots.actualBoundingBoxRight + dots.actualBoundingBoxLeft);
+    }
+
+    widthCalc = computed(() => this.measureText(this.text()));
 }
