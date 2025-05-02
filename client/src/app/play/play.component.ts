948872ba24d296d31b5103b01875652a6f2b2724
Justus Hibshman <justus.i.hibshman@proton.me>
2025-05-02 18:21:27 -0400

    Used the component logic to scale the play grid for all situations

--- client/src/app/play/play.component.ts
+++ client/src/app/play/play.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject, OnInit, signal, Signal } from '@angular/core';
+import { Component, computed, HostListener, inject, OnInit, signal, Signal } from '@angular/core';
 
 import { NavButtonComponent } from '@local-components/nav-button/nav-button.component';
 
@@ -19,26 +19,45 @@ export class PlayComponent implements OnInit {
     setup = inject(SetupService);
     gameplay = inject(GameplayService);
 
-    board = this.gameplay.getBoard();
+    rawBoard = this.gameplay.getBoard();
+    board = computed(() => this.safeBoard(this.rawBoard()));
     currentPlayer: Signal<number> = this.gameplay.getPlayer();
     winner = this.gameplay.getWinner();
     captures = this.gameplay.getCaptures();
 
     colors = ["empty", "B", "C", "F", "E", "D", "A"];
 
-    ngOnInit(): void {
+    screenWidth = signal<number>(0);
+    screenHeight = signal<number>(0);
+
+    borderWidth: number = 4;
+    // cellPadding: number = 10; -- not needed -- grid takes care of sizing cells
+
+    gridWidth  = computed(() => this.widthCalc(this.screenWidth(), this.screenHeight()) -
+                                    this.borderWidth * 2);
+    gridHeight = computed(() => this.heightCalc(this.screenWidth(), this.screenHeight()) -
+                                    this.borderWidth * 2);
+    // not needed -- grid takes care of sizing cells
+    // cellSize   = computed(() => this.cellCalc(this.screenWidth(), this.screenHeight()) -
+    //                                 (this.borderWidth + this.cellPadding) * 2);
+    gridStyle  = computed(() => "width: " + this.gridWidth() + "px; height: " + this.gridHeight() + 
+                                "px; grid-template-columns: repeat(" + this.numCols() + ", 1fr);");
+
+    numRows = computed(() => this.board().length);
+    numCols = computed(() => this.board()[0].length);
+
+    hoverX = signal<number | null>(null);
+    hoverY = signal<number | null>(null);
+
+    ngOnInit() {
+        this.screenWidth.set(window.innerWidth);
+        this.screenHeight.set(window.innerHeight);
     }
 
-    transposedBoard = computed(() => this.transpose(this.board()));
-    gravityStr = computed(() => this.gameplay.getGravity() ? "hoverable" : ""); 
-
-    transpose(a: Array<Array<number>> | undefined): Array<Array<number>> {
-        if (a === undefined || a.length == 0) {
-            return [];
-        }
-        return Array.from({length: a[0].length}, (x, i) =>
-                          Array.from({length: a.length},
-                                     (y, j) => a[j][i]));
+    @HostListener('window:resize', ['$event'])
+    onWindowResize() {
+        this.screenWidth.set(window.innerWidth);
+        this.screenHeight.set(window.innerHeight);
     }
 
     selectSpot(m: Move): void {
@@ -50,13 +69,13 @@ export class PlayComponent implements OnInit {
             return;
         }
 
-        let b: Array<Array<number>> | undefined = this.board();
-        let numRows = b === undefined ? 0 : b.length;
+        let numRows = this.numRows();;
 
         for (let r = numRows - 1; r >= 0; r--) {
             let m2 = {row: r, col: m.col};
             if (this.isLegalMove(m2)) {
                 this.gameplay.makeMove(m2);
+                this.endHover();
                 return;
             }
         }
@@ -65,5 +84,66 @@ export class PlayComponent implements OnInit {
     isLegalMove(m: Move): boolean {
         return this.gameplay.isLegalMove(m) &&
                this.gameplay.isLocalPlayer(this.currentPlayer());
+    }
+
+    widthCalc(screenWidth: number, screenHeight: number): number {
+        return Math.round(this.rawCellCalc(screenWidth, screenHeight) * this.numCols()) + 2 * this.borderWidth;
+    }
+
+    heightCalc(screenWidth: number, screenHeight: number): number {
+        return Math.round(this.rawCellCalc(screenWidth, screenHeight) * this.numRows()) + 2 * this.borderWidth;
+    }
+
+    cellCalc(screenWidth: number, screenHeight: number): number {
+        return Math.floor(this.rawCellCalc(screenWidth, screenHeight));
+    }
+
+    rawCellCalc(screenWidth: number, screenHeight: number) {
+        let maxWidth = screenWidth - (160 + 2 * this.borderWidth);
+        let maxHeight = screenHeight - 2 * this.borderWidth;
+
+        let widthLimited  = maxWidth / this.numCols();
+        let heightLimited = maxHeight / this.numRows();
+        return Math.min(widthLimited, heightLimited);
+    }
+
+    beginHover(m: Move): void {
+        if (this.gameplay.getGravity()) {
+            this.hoverY.set(null);
+            let b = this.board();
+            for (let r = b.length - 1; r >= 0; r--) {
+                if (this.isLegalMove({row: r, col: m.col})) {
+                    this.hoverX.set(m.col);
+                    this.hoverY.set(r);
+                    return;
+                }
+            }
+        }
+        else if (this.isLegalMove(m)) {
+            this.hoverX.set(m.col);
+            this.hoverY.set(m.row);
+            return;
+        }
+        this.endHover();
+    }
+
+    endHover(): void {
+        this.hoverX.set(null);
+        this.hoverY.set(null);
+    }
+
+    hoverAs(shouldHover: boolean) {
+        if (shouldHover) {
+            return this.colors[this.currentPlayer() + 1];
+        } else {
+            return "";
+        }
+    }
+
+    safeBoard(b: Array<Array<number>> | undefined): Array<Array<number>> {
+        if (b === undefined) {
+            return [[]];
+        }
+        return b;
     }
 }
