ee77bf70f4ccc9dccbbe0aa1f4c32159320fd6f5
Justus Hibshman <justus.i.hibshman@proton.me>
2025-06-03 16:38:02 -0400

    Simple proof of concept command for querying postgres database

---
+++ backend/go-code/cmd/database-test/main.go
@@ -0,0 +1,70 @@
+package main
+ 
+import (
+    "database/sql"
+    "encoding/json"
+    "fmt"
+    _ "github.com/lib/pq"
+    "net/http"
+    "os"
+    "time"
+)
+ 
+const (
+    host     = "localhost"
+    port     = 5432
+    user     = "postgres"
+    dbname   = "postgres"
+)
+ 
+func main() {
+    password_file := os.Getenv("POSTGRES_PASSWORD_FILE")
+    password, err := os.ReadFile(password_file)
+    CheckError(err)
+    passwordString := string(password)
+
+    // connection string
+    psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+                            host, port, user, passwordString, dbname)
+
+    // open database
+    db, err := sql.Open("postgres", psqlconn)
+    CheckError(err)
+
+    // close database (eventually)
+    defer db.Close()
+ 
+    // check db
+    err = db.Ping()
+    CheckError(err)
+
+    var currentTime string
+    currentTime = "Now"
+    handler := func(w http.ResponseWriter, r *http.Request) {stringPasser(w, r, &currentTime)}
+
+    http.HandleFunc("/time", handler)
+    go http.ListenAndServe(":8080", nil)
+
+    for {
+        rows, latestErr := db.Query("SELECT CURRENT_TIME;")
+        CheckError(latestErr)
+        for rows.Next() {
+            rows.Scan(&currentTime)
+        }
+        rows.Close()
+        time.Sleep(1 * time.Second)
+    }
+ 
+}
+
+func stringPasser(w http.ResponseWriter, r *http.Request, text *string) {
+    w.WriteHeader(http.StatusOK)
+    marshaled, _ := json.Marshal(*text)
+    w.Write(marshaled)
+}
+ 
+func CheckError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
--- backend/go-code/go.mod
+++ backend/go-code/go.mod
@@ -3,3 +3,5 @@ module linegames/backend
 replace linegames/backend => ./
 
 go 1.22.2
+
+require github.com/lib/pq v1.10.9 // indirect
