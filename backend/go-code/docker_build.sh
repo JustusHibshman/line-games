39fee1f7052db8d46c346239c01b5757b8289c4e
Justus Hibshman <justus.i.hibshman@proton.me>
2025-06-20 18:18:00 -0400

    More builds

--- backend/go-code/docker_build.sh
+++ backend/go-code/docker_build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-docker build --target db-test -t database_test:0.0.17 .
 docker login --username justushibshman
-docker tag      database_test:0.0.17 justushibshman/jih_personal:database_test-0.0.17
-docker push                          justushibshman/jih_personal:database_test-0.0.17
+docker build --target db-test -t database_test:0.0.18 .
+docker tag      database_test:0.0.18 justushibshman/jih_personal:database_test-0.0.18
+docker push                          justushibshman/jih_personal:database_test-0.0.18
+
+docker build --target lobby-server -t lobby_server:0.0.1 .
+docker tag      lobby_server:0.0.1 justushibshman/jih_personal:lobby_server-0.0.1
+docker push                        justushibshman/jih_personal:lobby_server-0.0.1
+
+docker build --target data-cleanup -t data_cleanup:0.0.1 .
+docker tag      data_cleanup:0.0.1 justushibshman/jih_personal:data_cleanup-0.0.1
+docker push                        justushibshman/jih_personal:data_cleanup-0.0.1
