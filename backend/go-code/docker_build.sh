84d5d440d08879d91a222d548b263d7fccdb073e
Justus Hibshman <justus.i.hibshman@proton.me>
2025-07-01 16:42:24 -0400

    config file image version numbers

--- backend/go-code/docker_build.sh
+++ backend/go-code/docker_build.sh
@@ -6,14 +6,14 @@ docker build --target db-test -t database_test:0.0.18 .
 docker tag      database_test:0.0.18 justushibshman/jih_personal:database_test-0.0.18
 docker push                          justushibshman/jih_personal:database_test-0.0.18
 
-docker build --target setup-server -t setup_server:0.1.5 .
-docker tag      setup_server:0.1.5 justushibshman/jih_personal:setup_server-0.1.5
-docker push                        justushibshman/jih_personal:setup_server-0.1.5
+docker build --target setup-server -t setup_server:0.1.10 .
+docker tag      setup_server:0.1.10 justushibshman/jih_personal:setup_server-0.1.10
+docker push                         justushibshman/jih_personal:setup_server-0.1.10
 
-docker build --target lobby-server -t lobby_server:0.1.0 .
-docker tag      lobby_server:0.1.0 justushibshman/jih_personal:lobby_server-0.1.0
-docker push                        justushibshman/jih_personal:lobby_server-0.1.0
+docker build --target lobby-server -t lobby_server:0.1.3 .
+docker tag      lobby_server:0.1.3 justushibshman/jih_personal:lobby_server-0.1.3
+docker push                        justushibshman/jih_personal:lobby_server-0.1.3
 
-docker build --target data-cleanup -t data_cleanup:0.1.0 .
-docker tag      data_cleanup:0.1.0 justushibshman/jih_personal:data_cleanup-0.1.0
-docker push                        justushibshman/jih_personal:data_cleanup-0.1.0
+docker build --target data-cleanup -t data_cleanup:0.1.3 .
+docker tag      data_cleanup:0.1.3 justushibshman/jih_personal:data_cleanup-0.1.3
+docker push                        justushibshman/jih_personal:data_cleanup-0.1.3
