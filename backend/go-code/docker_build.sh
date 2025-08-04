6f2137eb9f3a19c929c5dc07034c70483f08298c
Justus Hibshman <justus.i.hibshman@proton.me>
2025-08-04 16:54:27 -0400

    Changed build to do more locally - less image up- and down-loading

--- backend/go-code/docker_build.sh
+++ backend/go-code/docker_build.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-docker login --username justushibshman
+# Build this once to download the alpine image and the postgres golang library
+# docker build --target dependencies -t lg_backend_dependencies:1.0 .
 
-docker build --target db-test -t database_test:0.0.18 .
-docker tag      database_test:0.0.18 justushibshman/jih_personal:database_test-0.0.18
-docker push                          justushibshman/jih_personal:database_test-0.0.18
+# docker login --username justushibshman
 
-docker build --target setup-server -t setup_server:0.1.12 .
-docker tag      setup_server:0.1.12 justushibshman/jih_personal:setup_server-0.1.12
-docker push                         justushibshman/jih_personal:setup_server-0.1.12
+# docker build --target db-test -t database_test:0.0.18 .
+# docker tag      database_test:0.0.18 justushibshman/jih_personal:database_test-0.0.18
+# docker push                          justushibshman/jih_personal:database_test-0.0.18
 
-docker build --target lobby-server -t lobby_server:0.1.3 .
-docker tag      lobby_server:0.1.3 justushibshman/jih_personal:lobby_server-0.1.3
-docker push                        justushibshman/jih_personal:lobby_server-0.1.3
+docker build --target setup-server -t setup_server:0.1.22 .
+docker tag      setup_server:0.1.22 justushibshman/jih_personal:setup_server-0.1.22
+minikube image load justushibshman/jih_personal:setup_server-0.1.22
+# docker push                         justushibshman/jih_personal:setup_server-0.1.12
 
-docker build --target data-cleanup -t data_cleanup:0.1.3 .
-docker tag      data_cleanup:0.1.3 justushibshman/jih_personal:data_cleanup-0.1.3
-docker push                        justushibshman/jih_personal:data_cleanup-0.1.3
+# docker build --target lobby-server -t lobby_server:0.1.3 .
+# docker tag      lobby_server:0.1.3 justushibshman/jih_personal:lobby_server-0.1.3
+# minikube image load justushibshman/jih_personal:lobby_server-0.1.3
+  # docker push                        justushibshman/jih_personal:lobby_server-0.1.3
+
+# docker build --target data-cleanup -t data_cleanup:0.1.3 .
+# docker tag      data_cleanup:0.1.3 justushibshman/jih_personal:data_cleanup-0.1.3
+# minikube image load justushibshman/jih_personal:data_cleanup-0.1.3
+  # docker push                        justushibshman/jih_personal:data_cleanup-0.1.3
