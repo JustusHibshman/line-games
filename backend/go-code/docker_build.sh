5307520b242629930882fe09ff6a9348acf61f2d
Justus Hibshman <justus.i.hibshman@proton.me>
2025-08-13 13:18:11 -0400

    Beta build versions

--- backend/go-code/docker_build.sh
+++ backend/go-code/docker_build.sh
@@ -5,22 +5,22 @@
 
 docker login --username justushibshman
 
-docker build --target setup-server -t setup_server:0.2.3 .
-docker tag      setup_server:0.2.3 justushibshman/jih_personal:setup_server-0.2.3
-minikube image load justushibshman/jih_personal:setup_server-0.2.3
-docker push                        justushibshman/jih_personal:setup_server-0.2.3
+docker build --target setup-server -t setup_server:0.2.4 .
+docker tag      setup_server:0.2.4 justushibshman/jih_personal:setup_server-0.2.4
+minikube image load justushibshman/jih_personal:setup_server-0.2.4
+docker push                        justushibshman/jih_personal:setup_server-0.2.4
 
-# docker build --target lobby-server -t lobby_server:0.1.3 .
-# docker tag      lobby_server:0.1.3 justushibshman/jih_personal:lobby_server-0.1.3
-# minikube image load justushibshman/jih_personal:lobby_server-0.1.3
-# docker push                        justushibshman/jih_personal:lobby_server-0.1.3
+docker build --target lobby-server -t lobby_server:0.1.4 .
+docker tag      lobby_server:0.1.4 justushibshman/jih_personal:lobby_server-0.1.4
+minikube image load justushibshman/jih_personal:lobby_server-0.1.4
+docker push                        justushibshman/jih_personal:lobby_server-0.1.4
 
-# docker build --target data-cleanup -t data_cleanup:0.1.3 .
-# docker tag      data_cleanup:0.1.3 justushibshman/jih_personal:data_cleanup-0.1.3
-# minikube image load justushibshman/jih_personal:data_cleanup-0.1.3
-# docker push                        justushibshman/jih_personal:data_cleanup-0.1.3
+docker build --target data-cleanup -t data_cleanup:0.1.4 .
+docker tag      data_cleanup:0.1.4 justushibshman/jih_personal:data_cleanup-0.1.4
+minikube image load justushibshman/jih_personal:data_cleanup-0.1.4
+docker push                        justushibshman/jih_personal:data_cleanup-0.1.4
 
-# docker build --target gameplay-server -t gameplay_server:0.1.5 .
-# docker tag      gameplay_server:0.1.5 justushibshman/jih_personal:gameplay_server-0.1.5
-# minikube image load justushibshman/jih_personal:gameplay_server-0.1.5
-# docker push                        justushibshman/jih_personal:gameplay_server-0.1.5
+docker build --target gameplay-server -t gameplay_server:0.1.6 .
+docker tag      gameplay_server:0.1.6 justushibshman/jih_personal:gameplay_server-0.1.6
+minikube image load justushibshman/jih_personal:gameplay_server-0.1.6
+docker push                        justushibshman/jih_personal:gameplay_server-0.1.6
